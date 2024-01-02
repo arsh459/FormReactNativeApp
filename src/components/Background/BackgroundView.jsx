04b72ed3be93d7ddcb5b40ed237f52afd4875575
arsh459 <arshgoyal459@gmail.com>
2024-01-02 12:13:08 +0530

    custom background added

--- src/components/Background/BackgroundView.jsx
+++ src/components/Background/BackgroundView.jsx
@@ -1,9 +1,21 @@
-import { View } from "react-native"
+import { View, ImageBackground, Text } from "react-native"
 const BackgroundView = () => {
   return (
     <>
       <View className="absolute top-0 bottom-0 right-0 left-0 bg-[#EBF4FC] z-0"></View>
-      <View className="absolute top-0 left-0 right-0 h-[30%] bg-blue-500 z-0"></View>
+      <View className="absolute top-0 left-0 right-0 h-[30%] z-0">
+        <View className="w-full h-full">
+          <ImageBackground
+            source={{
+              uri: "https://images.pexels.com/photos/749564/pexels-photo-749564.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            }}
+            style={{
+              flex: 1,
+              resizeMode: "cover",
+            }}
+          />
+        </View>
+      </View>
     </>
   )
 }
